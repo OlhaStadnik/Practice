470de82420e3537ea08791c48d5acddf8b2b3804
Olha Stadnik <olgastadnik2507@gmail.comcd>
2024-11-27 16:07:58 +0200

    add_solution

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Task(models.Model):
+    content = models.TextField()
+    datetime = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(blank=True, null=True)
+    done = models.BooleanField(default=False)
+    tags = models.ManyToManyField("Tag", related_name="tasks", blank=True)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+
